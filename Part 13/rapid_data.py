726e8c1d088a75c3c205daacf4fd8badf033019e
iwantyourcodes <163874691+iwantyourcodes@users.noreply.github.com>
2024-06-12 10:41:33 -0400

    Add files via upload

---
+++ Part 13/rapid_data.py
@@ -0,0 +1,22 @@
+RAPID_DATA = [
+  {
+    #1
+    "range": 65,
+    "cooldown": 200,
+  },
+  {
+    #2
+    "range": 90,
+    "cooldown": 600,
+  },
+  {
+    #3
+    "range": 100,
+    "cooldown": 400,
+  },
+  {
+    #4
+    "range": 120,
+    "cooldown": 200,
+  }
+]
